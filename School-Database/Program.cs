8fb4c0bfbc7c9172d0d63aa9468e9b2950d71e76
Dimitar Atanasov <44848707+StageGreen@users.noreply.github.com>
2021-06-14 18:17:04 +0300

    Add project files.

---
+++ School-Database/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace School_Database
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
